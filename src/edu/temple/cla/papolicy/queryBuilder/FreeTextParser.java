b69e85587b4b6fca3edcc1b2eea698fc7ecdf808
wolfgang <wolfgang@temple.edu>
2014-12-15 08:28:25 -0500

    Moved FreeTextParser to QueryBuilder project

---
+++ src/edu/temple/cla/papolicy/queryBuilder/FreeTextParser.java
@@ -0,0 +1,70 @@
+package edu.temple.cla.papolicy.queryBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class FreeTextParser {
+
+    /**
+     * Create a Like expression to select a given key word.
+     * @param textColumn The column in the table to search.
+     * @param token The key word or phrase to be sought.
+     * @return
+     */
+    static Like createLike(String textColumn, String token) {
+        if (token.startsWith("\"")) {
+            token = token.substring(1, token.length() - 1);
+        }
+        return new Like(textColumn, "%" + token + "%");
+    }
+
+    /**
+     * Method to parse the free text and generate the query condition
+     *
+     * @param textColumn The name of the text column
+     * @param freeText the free text string
+     * @return The expressing to select the free text
+     */
+    public static Expression parse(String textColumn, String freeText) {
+        Pattern p = Pattern.compile("(\\\"[^\\\"]+\\\")|([^\\p{javaWhitespace}]+)");
+        Scanner scan = new Scanner(freeText);
+        List<String> list = new ArrayList<>();
+        String t;
+        while ((t = scan.findInLine(p)) != null) {
+            list.add(t);
+        }
+        Expression result;
+        if (list.size() == 1) {
+            result = FreeTextParser.createLike(textColumn, list.get(0));
+        } else {
+            result = new Conjunction();
+            for (String token : list) {
+                switch (token.toLowerCase()) {
+                    case "and":
+                        if (result.getClass() != Conjunction.class) {
+                            Expression lhs = result;
+                            Conjunction c = new Conjunction();
+                            c.addTerm(lhs);
+                            result = c;
+                        }
+                        break;
+                    case "or":
+                        if (result.getClass() != Disjunction.class) {
+                            Expression lhs = result;
+                            Disjunction d = new Disjunction();
+                            d.addTerm(lhs);
+                            result = d;
+                        }
+                        break;
+                    default:
+                        Composite c = (Composite) result;
+                        c.addTerm(FreeTextParser.createLike(textColumn, token));
+                }
+            }
+        }
+        return result;
+    }
+
+}
---
+++ test/edu/temple/cla/papolicy/queryBuilder/FreeTextParseTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.temple.cla.papolicy.queryBuilder;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Paul Wolfgang
+ */
+@RunWith(value=Parameterized.class)
+public class FreeTextParseTest {
+
+    private final String expected;
+    private final String textColumn;
+    private final String freeText;
+
+    @Parameters
+    public static Collection<String[]> getTextParameters() {
+        return Arrays.asList(new String[][] {
+            {"text LIKE(\'%foo%\')", "text", "foo"},
+            {"text LIKE(\'%foo%\') AND text LIKE(\'%bar%\')", "text", "foo bar"},
+            {"text LIKE(\'%foo%\') AND text LIKE(\'%bar%\')", "text", "foo and bar"},
+            {"(text LIKE(\'%foo%\') OR text LIKE(\'%bar%\'))", "text", "foo or bar"},
+            {"text LIKE(\'%foo bar%\')", "text", "\"foo bar\""},
+            {"(text LIKE(\'%foo bar%\') OR text LIKE(\'%baz%\'))", "text", "\"foo bar\" or baz"}
+        });
+    }
+
+    public FreeTextParseTest(String expected, String textColumn, String freeText) {
+        this.expected = expected;
+        this.textColumn = textColumn;
+        this.freeText = freeText;
+    }
+
+    @Test
+    public void testSingleToken() {
+        String result = FreeTextParser.parse(textColumn, freeText).toStringNoParen();
+        assertEquals(expected, result);
+    }
+
+}
